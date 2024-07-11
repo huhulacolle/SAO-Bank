9ed88096792d1b9fcaf706d97beea26f5f550186
huhulacolle <66556677+huhulacolle@users.noreply.github.com>
2024-07-11 11:43:01 +0200

    animation enderchest

--- src/main/java/org/sao/sao_bank/SAO_Bank.java
+++ src/main/java/org/sao/sao_bank/SAO_Bank.java
@@ -1,19 +1,18 @@
 package org.sao.sao_bank;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.sao.sao_bank.listener.EnderChestAnimation;
 
 import java.util.logging.Level;
 
-public final class SAO_Bank extends JavaPlugin implements Listener {
+public final class SAO_Bank extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().log(Level.INFO, "ça marche :D");
-        Bukkit.getPluginManager().registerEvents(this, this);
-
+        Bukkit.getPluginManager().registerEvents(new EnderChestAnimation(this), this);
     }
 
     @Override
---
+++ src/main/java/org/sao/sao_bank/listener/EnderChestAnimation.java
@@ -0,0 +1,72 @@
+package org.sao.sao_bank.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.metadata.FixedMetadataValue;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import org.sao.sao_bank.SAO_Bank;
+
+import java.util.Objects;
+
+public class EnderChestAnimation implements Listener {
+
+    private final SAO_Bank main;
+
+    public EnderChestAnimation(SAO_Bank main) {
+        this.main = main;
+    }
+
+    private void openGuiDebug(Player player) {
+        Inventory gui = Bukkit.createInventory(null, 18, "debug");
+        player.openInventory(gui);
+    }
+
+    @EventHandler
+    private void openAnimation(PlayerInteractEvent e) {
+
+        if (e.getClickedBlock().getType() == Material.ENDER_CHEST && e.getAction().isRightClick()) {
+            e.setCancelled(true);
+
+            this.openGuiDebug(e.getPlayer());
+
+            Location enderchestLocation = e.getPlayer().getTargetBlock(null, 5).getLocation();
+            e.getPlayer().setMetadata("openedEnderChestLocation", new FixedMetadataValue(main, enderchestLocation));
+
+            PacketContainer openPacket = new PacketContainer(PacketType.Play.Server.BLOCK_ACTION);
+            openPacket.getBlockPositionModifier().write(0, new BlockPosition(enderchestLocation.getBlockX(), enderchestLocation.getBlockY(), enderchestLocation.getBlockZ()));
+            openPacket.getIntegers().write(0, 1);
+            openPacket.getIntegers().write(1, 1);
+            ProtocolLibrary.getProtocolManager().sendServerPacket(e.getPlayer(), openPacket);
+
+            e.getPlayer().playSound(enderchestLocation, Sound.BLOCK_ENDER_CHEST_OPEN, 0.3f, 1.0f);
+        };
+    }
+
+    @EventHandler
+    private void closeAnimation(InventoryCloseEvent e) {
+        if (e.getView().getTitle().equals("debug")) {
+            Location enderchestLocation = (Location) e.getPlayer().getMetadata("openedEnderChestLocation").get(0).value();
+            e.getPlayer().removeMetadata("openedEnderChestLocation", main);
+
+            // Send packet to visually close the enderchest
+            PacketContainer closePacket = new PacketContainer(PacketType.Play.Server.BLOCK_ACTION);
+            closePacket.getBlockPositionModifier().write(0, new BlockPosition(enderchestLocation.getBlockX(), enderchestLocation.getBlockY(), enderchestLocation.getBlockZ()));
+            closePacket.getIntegers().write(0, 1);  // Type of block action (1 for chest)
+            closePacket.getIntegers().write(1, 0);  // Action ID (0 for close)
+            ProtocolLibrary.getProtocolManager().sendServerPacket((Player) e.getPlayer(), closePacket);
+
+            ((Player) e.getPlayer()).playSound(enderchestLocation, Sound.BLOCK_ENDER_CHEST_CLOSE, 0.3f, 1.0f);
+        }
+    }
+}
